c569c42a1ed9f48ed7792e13850b4db5077cdee9
NickMuhigi <n.muhigi@alustudent.com>
2023-11-02 11:40:56 -0700

    8-uppercase.py

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+offset = -32
+
+
+def uppercase(stru):
+    i = 0
+    while i < len(stru):
+        if islower(stru[i]):
+            holder = ord(stru[i]) + offset
+            new_str = chr(holder)
+        else:
+            new_str = stru[i]
+        print("{}".format(new_str), end='')
+        i = i + 1
+    print("")
+
+
+def islower(strs):
+    value = ord(strs)
+    if value >= 97 and value <= 122:
+        return True
+    else:
+        return False

de1c8c9df1a57544870e43a480a32086e0a495ac
NickMuhigi <n.muhigi@alustudent.com>
2023-11-05 08:34:43 -0800

    q

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = [row[:] for row in matrix]
     for idx, row in enumerate(new_matrix):
-        for idx2, col in enumerate(row):  
+        for idx2, col in enumerate(row):
             new_matrix[idx][idx2] = col ** 2
     return new_matrix

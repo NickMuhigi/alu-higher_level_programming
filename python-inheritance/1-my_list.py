1be0cae920b66f8476920764166649aa8aba96e1
NickMuhigi <n.muhigi@alustudent.com>
2023-11-24 09:23:51 -0800

    1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,27 +1,10 @@
 #!/usr/bin/python3
-# class 'Rectangle' that inherits from,
-# 'BaseGeometry' (7-base_geometry.py)
-"""
-    define a class 'Rectangle' inheriting from 'BaseGeometry'
-"""
-
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""defines class MyList"""
 
 
-class Rectangle(BaseGeometry):
-    """
-        representation of 'Rectangle'
-    """
+class MyList(list):
+    """class that inherits from list
+    with public instance method to print sorted list"""
 
-    def __init__(self, width, height):
-        """
-            intialize a new 'Rectangle'
-            Args:
-                width (int): width of the new rectangle
-                height (int): height of the new rectangle
-        """
-
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
+    def print_sorted(self):
+        print(sorted(self))

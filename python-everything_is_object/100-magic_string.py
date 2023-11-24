698c873e516dbba78baba4c876672136d0fa347c
NickMuhigi <n.muhigi@alustudent.com>
2023-11-24 06:40:58 -0800

    HEY

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
-def magic_string():
-    return ", ".join(["BestSchool"] * (i + 1) for i in range(30))
+def magic_string(times=[0]):
+    times[0] += 1
+    return "BestSchool" + (", BestSchool" * (times[0] - 1))
+
+# Test case 1
+print(magic_string())
+
+# Test case 2
+print(magic_string())
+print(magic_string())
+
+# Test case 3
+for i in range(30):
+    print(magic_string())

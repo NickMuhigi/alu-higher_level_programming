7b62337954b15eb4fbead92f5fb2bd76b9045fee
NickMuhigi <n.muhigi@alustudent.com>
2023-11-24 08:22:42 -0800

    K

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -8,7 +8,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """
-    Square class that inherits from Rectangle
+    Represents a Square, a specific type of Rectangle
     """
 
     def __init__(self, size):
@@ -18,3 +18,25 @@ class Square(Rectangle):
             size (int): The size of the square.
         """
         super().__init__(size, size)
+
+
+if __name__ == "__main__":
+    """
+    Test cases
+    """
+    s = Square(5)
+
+    print("Module documentation:", __doc__)
+    print("Class documentation:", Square.__doc__)
+    print("Directory of Square:", dir(s))
+    print("Is Square a subclass of Rectangle:", issubclass(Square, Rectangle))
+
+    try:
+        invalid_square = Square("13")
+    except TypeError as e:
+        print("Error creating a Square with invalid size:", e)
+
+    s = Square(13)
+    print("Area of the square:", s.area())
+    print("Width of the square:", s.width)
+    print("Height of the square:", s.height)

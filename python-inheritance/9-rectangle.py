0812093c10e8a5b32c2dc7c1a3646de4e1a41856
NickMuhigi <n.muhigi@alustudent.com>
2023-11-29 06:49:02 -0800

    9-rectangle.py

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
+""" triangle module importing geometry """
 
-""" Define base_geometry module """
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ Rectangle class body """
+    """ rectange class inherit from Base geometry """
 
     def __init__(self, width, height):
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
+        """ init class """
+        self.integer_validator("width", width)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
 
     def area(self):
-        return self.__width * self.__height
+        """ override implement area """
+        return (self.__width * self.__height)
 
     def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        """ area string represent """
+        return ("[Rectangle] {:d}/{:d}".format(self.__width, self.__height))

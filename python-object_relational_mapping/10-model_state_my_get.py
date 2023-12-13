1fb643c2a92adbed8d172dec09733060fb6a78fc
NickMuhigi <n.muhigi@alustudent.com>
2023-12-13 05:42:40 -0800

    N

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
-"""script that prints the State object with the name passed as argument
-from the database hbtn_0e_6_usa
-"""
 
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from model_state import Base, State
-
+""" 10-model_state_my_get module """
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+
+    from sqlalchemy import create_engine
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy.orm import sessionmaker
+    import sys
+    from model_state import Base, State
+
+    inp = sys.argv
+    if len(inp) < 5 or ";" in inp[4]:
+        exit(1)
+
+    conn_str = "mysql+mysqldb://{}:{}@localhost:3306/{}"
+    engine = create_engine(conn_str.format(inp[1], inp[2], inp[3]))
+    Session = sessionmaker(engine)
+
     Base.metadata.create_all(engine)
-    session = Session(engine)
 
-    state_name = sys.argv[4]
+    session = Session()
 
-    state = session.query(State).filter(State.name == state_name).first()
-    if state is None:
+    my_query = session.query(State).filter(State.name.like(inp[4])).all()
+
+    if len(my_query) == 0:
         print("Not found")
     else:
-        print(f'{state.id}')
+        print(my_query[0].id)
 
     session.close()

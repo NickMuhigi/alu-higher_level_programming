0c4dece547e9970eadd7f25e50ddcbba559cf4c2
NickMuhigi <n.muhigi@alustudent.com>
2024-01-29 03:04:34 -0800

    Q

---
+++ javascript-warm_up/100-let_me_const.js
@@ -0,0 +1,13 @@
+// ModifyVariable.js
+
+// Initial value of myVar
+let myVar = 123;
+
+// Display the original value
+console.log('Original myVar:', myVar);
+
+// Modify the value of myVar
+myVar = 333;
+
+// Display the modified value
+console.log('Modified myVar:', myVar);

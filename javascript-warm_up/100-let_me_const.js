fe1f3bd3b5c6c6bdbabe2c0de4b78e5218feb286
NickMuhigi <n.muhigi@alustudent.com>
2024-01-29 03:09:37 -0800

    E

--- javascript-warm_up/100-let_me_const.js
+++ javascript-warm_up/100-let_me_const.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-let myVar = 123;
-console.log('Original myVar:', myVar);
-myVar = 333;
-console.log('Modified myVar:', myVar);
+myVar = 89;
+require('./100-let_me_const');
+console.log(myVar);

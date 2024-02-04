07e72e3becb1982d5612b12a985be7b5589aa39c
NickMuhigi <n.muhigi@alustudent.com>
2024-02-04 07:00:19 -0800

    Z

--- javascript_objects_scopes_closures/100-map.js
+++ javascript_objects_scopes_closures/100-map.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
-/* 100-map.js */
-exports.converter = function (base)
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((item, index) => item * index));
